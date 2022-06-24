2adaa91b74cd89c2908ed32ad2fae0e93a1b4144
Kyle Chinick <kylechinick@icloud.com>
2022-06-23 19:04:10 -0700

    Add semi-colon, remove extra line breaks

--- __tests__/biz-func.test.js
+++ __tests__/biz-func.test.js
@@ -1,10 +1,7 @@
 import exampleTestFunction from '../src/js/biz-func.js';
 
-
-describe("exampleTestFunction", () => {
-
-  test("should....", () => {
-    expect(exampleTestFunction()).toEqual("hi there, from the deck")
+describe('exampleTestFunction', () => {
+  test('should....', () => {
+    expect(exampleTestFunction()).toEqual('hi there, from the deck');
   });
-
 });
